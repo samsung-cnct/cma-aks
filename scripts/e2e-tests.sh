2cb02c2492cbc7514bcd00f06b727684d23b75ed
David E Watson <davidewatson@gmail.com>
2019-01-16 13:49:01 -0800

    Add debug flag to all helm invocations.

--- scripts/e2e-tests.sh
+++ scripts/e2e-tests.sh
@@ -13,20 +13,20 @@ export KUBECONFIG=$(kind get kubeconfig-path)
 
 kubectl create clusterrolebinding superpowers --clusterrole=cluster-admin --user=system:serviceaccount:kube-system:default
 
-helm init --wait
+helm init --wait --debug
 
-helm install --name my-release stable/cert-manager --wait
-helm install --name nginx-ingress stable/nginx-ingress --wait
+helm install --name my-release stable/cert-manager --wait --debug
+helm install --name nginx-ingress stable/nginx-ingress --wait --debug
 
 # TODO: This is incorrect. We should be installing the repo containing the PR we 
 # wish to test.
 go get github.com/samsung-cnct/cma-aks
 cd ${GOPATH}/src/github.com/samsung-cnct/cma-aks
-helm install --name cma-aks deployments/helm/cma-aks/ --wait
+helm install --name cma-aks deployments/helm/cma-aks/ --wait --debug
 
 helm repo add cnct https://charts.migrations.cnct.io
-helm install --name cluster-manager-api cnct/cluster-manager-api --wait
-helm install --name cma-operator cnct/cma-operator --wait
+helm install --name cluster-manager-api cnct/cluster-manager-api --wait --debug
+helm install --name cma-operator cnct/cma-operator --wait --debug
 
 # TODO: Add tests here
 
